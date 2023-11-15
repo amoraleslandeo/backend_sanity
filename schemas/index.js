497fb593cdf9d374984ab7905665ff8a0e41f63e
amoraleslandeo <amoraleslandeo@gmail.com>
2023-11-15 19:41:00 -0300

    feat: backend sanity added

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,14 @@
-export const schemaTypes = []
+import testimonials from "./testimonials";
+import abouts from "./abouts";
+import brands from "./brands";
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import workExperiences from "./workExperience";
+import works from "./works";
+
+export const schemaTypes = [works, testimonials, brands, abouts, skills, workExperiences, experiences, contact];
+
+
+
+
